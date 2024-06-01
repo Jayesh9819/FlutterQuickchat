3eef8e97aa2e507a8b2c08116b09992aedfb7d5f
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-06-01 16:48:23 +0530

    Agent Vesion With the proper sound and files

--- lib/home.dart
+++ lib/home.dart
@@ -3,16 +3,16 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_app_installations/firebase_app_installations.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart'
-as webview_flutter_android;
+import 'package:webview_flutter_android/webview_flutter_android.dart' as webview_flutter_android;
 import 'package:image_picker/image_picker.dart' as image_picker;
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'network_service.dart';
-
 
 class PreviewWebpage extends StatefulWidget {
   const PreviewWebpage({super.key});
@@ -32,6 +32,7 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
   static const String channelId = 'high_importance_channel';
   static const String channelName = 'High Importance Notifications';
   static const String channelDescription = 'This channel is used for important notifications.';
+
   @override
   void initState() {
     super.initState();
@@ -52,6 +53,14 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
         onPageStarted: (String url) {},
         onPageFinished: (String url) {},
         onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) async {
+          if (request.url.contains('.jpeg') || request.url.contains('.pdf') || request.url.contains('.mp4') || request.url.contains('.gif')  || request.url.contains('.jpg') || request.url.contains('.png')) {
+            // Handle file download
+            await _launchURL(request.url);
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
       ))
       ..addJavaScriptChannel(
         'Flutter',
@@ -73,21 +82,25 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
       ..loadRequest(Uri.parse("https://quickchat.biz"));
   }
 
+  Future<void> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   initFilePicker() async {
     if (Platform.isAndroid) {
-      final androidController = (_controller.platform
-      as webview_flutter_android.AndroidWebViewController);
+      final androidController = (_controller.platform as webview_flutter_android.AndroidWebViewController);
       await androidController.setOnShowFileSelector(_androidFilePicker);
     }
   }
 
-
-  Future<List<String>> _androidFilePicker(
-      webview_flutter_android.FileSelectorParams params) async {
+  Future<List<String>> _androidFilePicker(webview_flutter_android.FileSelectorParams params) async {
     if (params.acceptTypes.any((type) => type == 'image/*')) {
       final picker = image_picker.ImagePicker();
-      final photo =
-      await picker.pickImage(source: image_picker.ImageSource.camera);
+      final photo = await picker.pickImage(source: image_picker.ImageSource.camera);
 
       if (photo == null) {
         return [];
@@ -95,24 +108,18 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
       return [Uri.file(photo.path).toString()];
     } else if (params.acceptTypes.any((type) => type == 'video/*')) {
       final picker = image_picker.ImagePicker();
-      final vidFile = await picker.pickVideo(
-          source: ImageSource.camera, maxDuration: const Duration(seconds: 10));
+      final vidFile = await picker.pickVideo(source: ImageSource.camera, maxDuration: const Duration(seconds: 10));
       if (vidFile == null) {
         return [];
       }
       return [Uri.file(vidFile.path).toString()];
     } else {
       try {
-        if (params.mode ==
-            webview_flutter_android.FileSelectorMode.openMultiple) {
-          final attachments =
-          await FilePicker.platform.pickFiles(allowMultiple: true);
+        if (params.mode == webview_flutter_android.FileSelectorMode.openMultiple) {
+          final attachments = await FilePicker.platform.pickFiles(allowMultiple: true);
           if (attachments == null) return [];
 
-          return attachments.files
-              .where((element) => element.path != null)
-              .map((e) => File(e.path!).uri.toString())
-              .toList();
+          return attachments.files.where((element) => element.path != null).map((e) => File(e.path!).uri.toString()).toList();
         } else {
           final attachment = await FilePicker.platform.pickFiles();
           if (attachment == null) return [];
@@ -124,6 +131,7 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
       }
     }
   }
+
   void _setupFirebase() {
     print("Setting up Firebase...");
     _messaging.requestPermission(
@@ -140,7 +148,6 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
 
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   }
-
 
   static Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
     print("Handling a background message: ${message.messageId}");
@@ -183,7 +190,6 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
     print("Notification channel created.");
   }
 
-
   void _getAndStoreFCMToken() async {
     print("Getting FCM token...");
     String? token = await FirebaseMessaging.instance.getToken();
@@ -201,22 +207,17 @@ class _PreviewWebpageState extends State<PreviewWebpage> {
     }
   }
 
-
   Future<void> _loadUserId() async {
     final prefs = await SharedPreferences.getInstance();
     _userId = prefs.getString('user_id') ?? '';
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: WebViewWidget(controller: _controller),
-
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
+      ),
     );
   }
-
-
 }
